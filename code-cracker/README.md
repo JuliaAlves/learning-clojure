522f253125c4c21bb1beddd21c933ec326bcca64
julia.arruda <julia.arruda@julia.arruda>
2023-04-04 14:29:54 -0300

    creating code cracker

---
+++ code-cracker/README.md
@@ -0,0 +1,1 @@
+Resolving https://codingdojo.org/kata/CodeCracker/
---
+++ code-cracker/src/code_cracker/main.clj
@@ -0,0 +1,41 @@
+(ns code-cracker.main 
+  (:require [clojure.string :as clojure.string]
+            [clojure.string :as s]))
+
+(def dictionary {
+"!" "a" 
+")" "b" 
+"\"" "c" 
+"(" "d" 
+"£" "e" 
+"*" "f" 
+"%" "g" 
+"&" "h" 
+">" "i" 
+"<" "j" 
+"@" "k" 
+"a" "l" 
+"b" "m" 
+"c" "n" 
+"d" "o" 
+"e" "p" 
+"f" "q" 
+"g" "r" 
+"h" "s" 
+"i" "t" 
+"j" "u" 
+"k" "v" 
+"l" "w" 
+"m" "x" 
+"n" "y" 
+"o" "z"
+" " " "})
+
+(defn get-real-char [char]
+  (or (get dictionary (str char))
+      char))
+
+(defn decrypt [input]
+  (let [decrypted-chars (map get-real-char input)
+        decrypted-str (s/join "" decrypted-chars)]
+    decrypted-str))
